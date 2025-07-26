c4736ecf38768d1cd013c9dd59968f855b9646e0
dineshkhatroth <dinesh.khatroth@thezennialpro.com>
2025-07-26 16:27:17 +0530

    Refactored validation logic into separate validators.py and updated user models

--- user_registration_API/app/models/user_models.py
+++ user_registration_API/app/models/user_models.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel,Field,EmailStr,field_validator
-import re
-
-
+from pydantic import BaseModel, Field, EmailStr, field_validator
+from typing import Optional
+from app.utils.validators import validate_mobile, validate_email_domain,validate_password_strength
 
 class UserRegister(BaseModel):
     email: EmailStr
@@ -14,30 +13,32 @@ class UserRegister(BaseModel):
     password: str = Field(min_length=8, max_length=20)
     active: bool
 
+    @field_validator('email')
+    def email_domain(cls, v): 
+        return validate_email_domain(v)
 
     @field_validator('mobile')
-    def validate_mobile(cls,v):
-      pattern=r'^[6-9]\d{9}$'
-      if not re.fullmatch(pattern,v):
-        raise ValueError("Mobile number must be a valid 10-digit number.")
-      return v
-    
+    def mobile_check(cls, v): 
+        return validate_mobile(v)
+
     @field_validator('password')
-    def validate_password_strength(cls, v):
-        pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,20}$'
-        if not re.fullmatch(pattern, v):
-            raise ValueError("Password must include uppercase, lowercase, number, and special character.")
-        return v
-    
-    @field_validator('email')
-    def validate_email_domain(cls, v):
-        v = v.lower()
-        if not v.endswith(('gmail.com')):
-            raise ValueError("Only gmail.com  domains are allowed.")
-        return v
-    
+    def password_check(cls, v): 
+        return validate_password_strength(v)
+
 
 class LoginRequest(BaseModel):
-   username:str
-   password:str = Field(min_length=8, max_length=20)
+    username: str
+    password: str = Field(min_length=8, max_length=20)
 
+
+class ChangeContactRequest(BaseModel):
+    email: Optional[EmailStr] = None
+    mobile: Optional[str] = None
+
+    @field_validator('email')
+    def email_domain(cls, v): 
+        return validate_email_domain(v)
+
+    @field_validator('mobile')
+    def mobile_check(cls, v): 
+        return validate_mobile(v)
