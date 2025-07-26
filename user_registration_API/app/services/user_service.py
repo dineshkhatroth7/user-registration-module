e4c66c029a3ddd125e1575cafb4efdc68ef587a1
dineshkhatroth <dinesh.khatroth@thezennialpro.com>
2025-07-26 11:10:56 +0530

    Added password hashing using bcrypt during user registration

--- user_registration_API/app/services/user_service.py
+++ user_registration_API/app/services/user_service.py
@@ -4,6 +4,10 @@ from app.db.mongo import users_collection
 from app.utils.logger import logger  
 from app.utils.exceptions import UserAlreadyExistsException
 from datetime import datetime
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def register_user_service(request: UserRegister):
     logger.info(f"Received registration request for {request.email},{request.mobile}")
@@ -20,6 +24,7 @@ def register_user_service(request: UserRegister):
         raise UserAlreadyExistsException()
 
     user_data = request.model_dump()
+    user_data["password"] = pwd_context.hash(user_data["password"])
     user_data["dob"] = datetime.strptime(user_data["dob"], "%Y-%m-%d")
     user_data["doj"] = datetime.strptime(user_data["doj"], "%Y-%m-%d")
     users_collection.insert_one(user_data)
